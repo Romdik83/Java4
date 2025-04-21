d176bfecd805c0c86a75ba7ef6622e6f9994aa26
Romdik <romdik83@gmail.com>
2025-04-21 17:21:23 +0300

    change src/BonusMilesService.java

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+
+    public int calculate(int price, int amount, int theMile) {
+        int result;
+        result = price / amount * theMile;
+     return  result;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10000;
+        int amount = 20;
+        int theMiles = 1;
+
+        System.out.println();
+        System.out.println(service.calculate(10000, 20, 1));
+
+        System.out.println();
+        System.out.println(service.calculate(16254, 20, 1));
+
+        System.out.println();
+        System.out.println(service.calculate(18000, 20, 1));
     }
 }
