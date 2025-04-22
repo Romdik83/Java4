ffe1fb2e6c6169a75c08972dd1b1aabfc2691417
Romdik <romdik83@gmail.com>
2025-04-22 17:18:54 +0300

    cheange bonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,8 @@
 public class BonusMilesService {
-    int amount = 20;
-    int theMile = 1;
-
-
     public int calculate(int price) {
         int result;
-        result = price / amount * theMile;
-     return  result;
+        result = price / 20 * 1;
+        return result;
 
     }
 }
